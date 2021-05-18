4ce539d96bfef9fb8f7c1abc830b32dc068baa06
CICERON Jeiel <rsma.simplon21@gmail.com>
2021-05-17 22:33:51 -0400

    evaluation stage le date du jour

---
+++ regionmaximini.com/includes/tables.php
@@ -0,0 +1,57 @@
+<div class="text-right">
+<button type="button" class="btn btn-success">Ajouter</button> 
+<button type="button" class="btn btn-success"><a class="importer" href="champ.php">importer</a></button> 
+</div><br>
+
+
+<table action="region.php" id="example" class="table table-striped table-bordered" style="width:100%">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Date</th>
+            <th>Site Web et Url</th>
+            <th>Pays</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>01</td>
+            <td>2020/04/25</td>
+            <td>YouTube https://www.youtube.com/</td>
+            <td>Etats-Unis</td>
+            <td><button type="button" class="btn btn-primary"><a href="region.php">editer</a></button>
+            <button type="button"class="btn btn-danger">Supprimer</button> </td>
+        </tr>
+        <tr>
+            <td>02</td>
+            <td>2020/04/25</td>
+            <td>W3schools https://www.w3schools.com/</td>
+            <td>Etats-Unis</td>
+            <td><button type="button" class="btn btn-primary"><a href="region.php">editer</a></button>
+            <button type="button"class="btn btn-danger">Supprimer</button> </td>
+        </tr>
+        <tr>
+            <td>03</td>
+            <td>2020/05/21</td>
+            <td>Louis Vuitton https://fr.louisvuitton.com/fra-fr/homepage</td>
+            <td>France</td>
+            <td><button type="button" class="btn btn-primary"><a href="region.php">editer</a></button>
+            <button type="button"class="btn btn-danger">Supprimer</button> </td>
+        </tr>
+        <tr>
+            <td>04</td>
+            <td>2020/05/22</td>
+            <td>Steam https://store.steampowered.com/?l=french</td>
+            <td>Etats-Unis</td>
+            <td><button type="button" class="btn btn-primary"><a href="region.php">editer</a></button>
+            <button type="button"class="btn btn-danger">Supprimer</button> </td>
+        </tr>
+        
+
+    </tbody>
+    
+    <tfoot>
+
+    </tfoot>
+</table>
